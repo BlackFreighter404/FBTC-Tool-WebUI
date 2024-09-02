38cbf9a6f1b1e393906787b748635277bc9c02df
BlackFreighter404 <BlackFreighter404@pissmail.com>
2024-09-02 17:01:50 +0800

    chore: Update Router component to use exact path for routes

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import Display from "./components/Display/Display";
 import Operation from "./components/Operation/Operation";
 import Request from "./components/Request/Request";
 
 function Router() {
+    const basename = process.env.PUBLIC_URL;
+
     return (
         <Routes>
-            <Route exact path="/" element={<Display />} />
-            <Route exact path="/display" element={<Display />} />
-            <Route exact path="/operation" element={<Operation />} />
-            <Route exact path="/request" element={<Request />} />
+            <Route exact path={`${basename}/`} element={<Display />} />
+            <Route exact path={`${basename}/display`}element={<Display />} />
+            <Route exact path={`${basename}/operation`} element={<Operation />} />
+            <Route exact path={`${basename}/request`} element={<Request />} />
         </Routes>
     );
 }
