5a3911616ea8763a8ae200169866ba0da0549f36
BlackFreighter404 <BlackFreighter404@pissmail.com>
2024-09-10 13:40:01 +0800

    feat: Add BOB to Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,10 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet } from 'wagmi/chains';
+import { mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet, bob } from 'wagmi/chains';
 
 const config = getDefaultConfig({
     appName: 'FBTC-Tool',
     projectId: '366ed824723a0776affcd32ceab77ad6',
-    chains: [mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet],
+    chains: [mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet, bob],
 });
 
 export default config;
