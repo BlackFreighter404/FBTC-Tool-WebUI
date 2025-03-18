917cb5b42d36a74f6d9e195f3c8a3591b51ae0a7
Moon <lmy375@139.com>
2025-03-18 19:24:52 +0800

    feat: add corn chain support

--- src/Config.js
+++ src/Config.js
@@ -3,34 +3,62 @@ import { mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet, bo
 
 import { defineChain } from 'viem';
 
+const sonic = defineChain({
+  id: 146,
+  name: 'Sonic',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Sonic',
+    symbol: 'S',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.soniclabs.com'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Sonic Explorer',
+      url: 'https://sonicscan.org/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 60,
+    },
+  },
+  testnet: false,
+});
+
+const corn = defineChain({
+  id: 21000000,
+  name: 'Corn',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Bitcorn',
+    symbol: 'BTCN',
+  },
+  rpcUrls: {
+    default: { http: ['https://mainnet.corn-rpc.com'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Corn Explorer',
+      url: 'https://cornscan.io/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 60,
+    },
+  },
+  testnet: false,
+})
+
 const config = getDefaultConfig({
     appName: 'FBTC-Tool',
     projectId: '366ed824723a0776affcd32ceab77ad6',
-    chains: [mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet, bob, defineChain({
-        id: 146,
-        name: 'Sonic',
-        nativeCurrency: {
-          decimals: 18,
-          name: 'Sonic',
-          symbol: 'S',
-        },
-        rpcUrls: {
-          default: { http: ['https://rpc.soniclabs.com'] },
-        },
-        blockExplorers: {
-          default: {
-            name: 'Sonic Explorer',
-            url: 'https://sonicscan.org/',
-          },
-        },
-        contracts: {
-          multicall3: {
-            address: '0xca11bde05977b3631167028862be2a173976ca11',
-            blockCreated: 60,
-          },
-        },
-        testnet: false,
-      })],
+    chains: [mainnet, sepolia, arbitrum, bsc, base, mantle, mantleSepoliaTestnet, bob, sonic, corn],
 });
 
 export default config;
